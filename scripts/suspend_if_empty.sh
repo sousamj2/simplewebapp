3a6578456dace1c9907e1dd4994e054b9470f789
Mário LEGION <mdacunha@cern.ch>
2026-05-06 21:08:39 +0100

    fix: update python interpreter path in suspend_if_empty.sh

--- scripts/suspend_if_empty.sh
+++ scripts/suspend_if_empty.sh
@@ -2,7 +2,7 @@
 cd /var/www/appmodules/simplewebapp/Funhelpers || exit 1
 PATH=/usr/local/bin:/usr/bin:/bin
 
-/var/www/appmodules/simplewebapp/app-env/bin/python /var/www/appmodules/simplewebapp/Funhelpers/suspend_if_empty.py \
+/var/www/appmodules/app-env/bin/python /var/www/appmodules/simplewebapp/Funhelpers/suspend_if_empty.py \
   --instance mcserver-mem8 \
   --zone europe-west1-b \
   --state-file /var/www/appmodules/simplewebapp/scripts/suspend_if_empty_state.json \
